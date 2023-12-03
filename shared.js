be5de5cfec33d107e3d6c8b1a430a2216feca786
Auron <98396887+auronvila@users.noreply.github.com>
2023-12-03 11:17:37 +0300

    added animations to the page

--- shared.js
+++ shared.js
@@ -7,7 +7,10 @@ let selectPlanButtons = document.querySelectorAll('.plan .button');
 
 selectPlanButtons.forEach(item => item.addEventListener('click', () => {
   modal.classList.add('open');
-  backdrop.classList.add('open');
+  backdrop.style.display = 'block';
+  setTimeout(() => {
+    backdrop.classList.add('open');
+  }, 200)
 }));
 
 negativeButton?.addEventListener('click', closeModal)
@@ -18,7 +21,10 @@ toggleButton.addEventListener('click', () => {
 
   // backdrop.className = 'open' this will override the whole class of the backdrop
   mobileNav.classList.add('open');
-  backdrop.classList.add('open');
+  backdrop.style.display = 'block';
+  setTimeout(() => {
+    backdrop.classList.add('open');
+  }, 200)
 });
 
 backdrop.addEventListener('click', () => {
@@ -29,6 +35,9 @@ function closeModal() {
   // backdrop.style.display = 'none'
   // modal.style.display = 'none'
   modal?.classList.remove('open');
-  backdrop.classList.remove('open');
   mobileNav.classList.remove('open');
+  backdrop.classList.remove('open');
+  setTimeout(() => {
+    backdrop.style.display = 'none';
+  }, 200)
 }
